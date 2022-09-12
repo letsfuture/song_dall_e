4f7ea1c6a5a10e717329f930b2927db23458d809
letsfuture <future220616@gmail.com>
2022-09-12 17:07:39 +0900

    포스트 목록, 상세 페이지에서 대표 이미지 없을 때 문제 해결하기

--- blog/urls.py
+++ blog/urls.py
@@ -13,16 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('blog/', include('blog.urls')),
-    path('admin/', admin.site.urls),
-    path('', include('single_pages.urls')),
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('', views.PostList.as_view()),
+    # path('<int:pk>/', views.single_post_page),
+    # path('', views.index),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
